51a5be4886f4bfcc620c0f7f0b73ba760a80dd78
Youshi <youshigouali@gmail.com>
2026-05-23 20:27:27 +0100

    avant commancer d'utiliser react

---
+++ dashboard.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+def get_patients():
+    with open("Patients.json", "r", encoding="utf-8") as f:
+        return json.load(f)
+
+@app.route("/")
+def dashboard():
+    patients = get_patients()
+    return render_template("dashboard.html", patients=patients)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
